4a8a963d21486fa1823fe7bd46b3d2e453a6c26f
18107 <18107@users.noreply.github.com>
2023-12-02 11:11:44 +1030

    Add files via upload

---
+++ CameraMode_Pilot.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+
+namespace ExteriorZoomSave
+{
+    [HarmonyPatch(typeof(PLCameraMode_Pilot), "Tick")]
+    internal class CameraMode_Pilot
+    {
+        private static float distance = 220f;
+
+        static void Prefix(ref float ___distance, ref float ___smoothDistance)
+        {
+            ___distance = distance;
+
+            if (___smoothDistance == 100f)
+            {
+                ___smoothDistance = distance;
+            }
+        }
+
+        static void Postfix(float ___distance)
+        {
+            distance = ___distance;
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace ExteriorZoomSave
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "Remembers the last zoom level when entering the pilot's seat";
+
+        public override string Name => "Exterior Zoom Save";
+
+        public override string ModID => "exteriorzoomsave";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.exteriorzoomsave";
+        }
+    }
+}
